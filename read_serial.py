ab60989e52c4b2636c7960d97af79c3fbbf55a54
Jaturaphum Suaprakhon <106952984+Jaturaphum@users.noreply.github.com>
2022-11-08 16:51:23 +0700

    Update read_serial.py

--- read_serial.py
+++ read_serial.py
@@ -1,15 +1,9 @@
 import serial,time
-while True:
-    try : 
-        ser = serial.Serial('/dev/ttyACM0', 115200, timeout=1)
-        break
-    except :
-        print("Waiting to stop working...")
-        time.sleep(0.3);
 
+ser = serial.Serial('/dev/ttyACM0', 115200, timeout=1)
 while True: 
     if ser.inWaiting() > 0:
         msg = ser.read(ser.inWaiting())
         ser.flushInput()
         print(msg.decode())
-    time.sleep(0.3)
+    time.sleep(0.2)
